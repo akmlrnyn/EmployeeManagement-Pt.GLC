e13a9de4798825a20a512bbed1d8d4eeb074e982
akmlrnyn <akmalranyan@gmail.com>
2024-01-25 13:07:24 +0700

    made read and store operation

---
+++ app/Http/Controllers/SalarySlipsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use App\Models\SalarySlip;
+use Illuminate\Http\Request;
+
+class SalarySlipsController extends Controller
+{
+    public function index() {
+        $slips = SalarySlip::all();
+        $employees = Employee::all();
+
+        return view('pages.salary-slips', compact('slips', 'employees'));
+    }
+
+    public function store(Request $request) {
+        $data = $request->except(['_token']);
+        SalarySlip::create($data);
+
+        return redirect('/employees');
+    }
+}
--- app/Models/SalarySlip.php
+++ app/Models/SalarySlip.php
@@ -9,6 +9,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class SalarySlip extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        '_token',
+        'employee_id',
+        'leave_request',
+        'overtime',
+        'late',
+        'tax',
+        'salary',
+        'month',
+    ];
     public function employee(): BelongsTo
     {
         return $this->belongsTo(Employee::class, 'employee_id', 'id');
