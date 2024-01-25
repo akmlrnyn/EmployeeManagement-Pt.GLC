711b6dd3594af6dda78a2e86fd034d986a28a24d
akmlrnyn <akmalranyan@gmail.com>
2024-01-25 13:07:56 +0700

    added month column in salary_slips table

---
+++ database/migrations/2024_01_25_033643_add_month_to_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('salary_slips', function (Blueprint $table) {
+            $table->enum('month', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])->after('late');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('salary_slips', function (Blueprint $table) {
+            $table->enum('month', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])->after('late');
+        });
+    }
+};
