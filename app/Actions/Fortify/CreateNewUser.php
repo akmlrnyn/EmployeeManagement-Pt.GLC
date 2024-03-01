6655c4d1fd74c33d3c231bed77b8efb7a851a1d2
mfathirr <mulfathir632@gmail.com>
2024-03-01 10:02:19 +0700

    user can input phone on register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -23,6 +23,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
+            'phone' => ['required', 'unique:users'],
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
@@ -30,6 +31,8 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'phone' => $input['phone'],
+            'secondary_phone' => $input['secondary_phone'],
         ]);
     }
 }
--- database/migrations/2024_03_01_025915_drop_column_phone_from_table_users.php
+++ database/migrations/2024_03_01_025915_drop_column_phone_from_table_users.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('phone');
-            $table->integer('secondary_phone')->nullable();
+            $table->dropColumn('phone');
+            $table->dropColumn('secondary_phone');
         });
     }
 
